3f0aca856a95a29bb2d6feb737303cab43f505a4
niknikich <82ann8@mail.ru>
2020-11-16 12:44:40 +0300

    edit const

--- src/module/youtrack-ds/youtrack-ds.const.ts
+++ src/module/youtrack-ds/youtrack-ds.const.ts
@@ -2,7 +2,7 @@ export const YOUTRACK_DS_KEY = 'youtrack_dataserver';
 export const USER_LIST_FIELDS = 'id,fullName,ringId';
 export const PROJECT_LIST_FIELDS = 'id,name,hubResourceId';
 export const ISSUE_LIST_FIELDS =
-  'id,,idReadable,summary,parent(issues(id,summary)),updater(id,fullName),project(id,name,hubResourceId),' +
+  'id,idReadable,summary,parent(issues(id,summary)),updater(id,fullName),project(id,name,hubResourceId),' +
   'customFields(id,name,value(name,id,localizedName,fullName,login,avatarUrl,isResolved,minutes,presentation))';
 export const ISSUE_LIST_QUERY_DAY = 'updated:Yesterday';
 export const ISSUE_LIST_QUERY_WEEK = 'updated:{Last week}';
